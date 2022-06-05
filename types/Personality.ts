c0e04f31d6c993e3ef7713194337fb0a70667c51
juliagulliksson <Julia.Gulliksson@yh.nackademin.se>
2022-06-05 11:48:55 +0200

    Add Personality type array

---
+++ types/Personality.ts
@@ -0,0 +1,30 @@
+type Personality = {
+  name: string;
+  points: number;
+};
+
+const personalities: Personality[] = [
+  {
+    name: "Neat",
+    points: 0,
+  },
+  {
+    name: "Outgoing",
+    points: 0,
+  },
+  {
+    name: "Active",
+    points: 0,
+  },
+
+  {
+    name: "Playful",
+    points: 0,
+  },
+  {
+    name: "Nice",
+    points: 0,
+  },
+];
+
+export default personalities;
